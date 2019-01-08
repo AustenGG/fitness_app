9a48d0fff64b82eb328da10f497dbeeb47bffce2
Jordan Bailey <jordanbailey@Jordans-MacBook-Pro.local>
2019-01-08 10:11:40 +0000

    structure for webpages, 404 error page for user enters unkown web addres

--- app.js
+++ app.js
@@ -5,9 +5,24 @@ const port = 8080;
 
 app.use(express.static(__dirname + '/views'));
 
-app.get('/', (req, res) => res.send({"code":400, "success":"Server running"}));
+//app.get('/', (req, res) => res.send({"code":400, "success":"Server running"}));
 // if you want to view a hmtl file
 // app.get('/page', (req, res) => res.sendFile(__dirname + '/views/page.html'));
-
+app.get('/BMI', function(req, res) {
+  res.render('BMI.ejs');
+});
+app.get('/calorie_counter', function(req, res) {
+  res.render('calorie_counter.ejs');
+});
+app.get('/', function(req, res) {
+  res.render('home.ejs');
+});app.get('/water_intake', function(req, res) {
+  res.render('water_intake.ejs');
+});app.get('/workout', function(req, res) {
+  res.render('workout.ejs');
+});
+app.get('*', function(req, res) {
+  res.render('error.ejs');
+});
 
 app.listen(port, () => console.log(`The app is running on port: ${port}! Make sure to open it in your browser!`));
