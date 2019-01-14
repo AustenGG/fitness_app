6e2f38f2752f03f32329c003a9e66ffaf0a63ebb
Jordan Bailey <jordanbailey@Jordans-MacBook-Pro.local>
2019-01-14 12:03:39 +0000

    navbar rendered as partial

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/views'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.set('view engine', 'ejs');
 //GET requests
 app.get('/BMI', function(req, res) {
   if (req.session.username == null) {
